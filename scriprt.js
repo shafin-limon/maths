ab19ab2cb7811e9d74eba283a8fcfe00f7cce342
Shafin-Limon <shafinlimon123.com>
2021-10-28 20:55:52 +0600

    math

---
+++ scriprt.js
@@ -0,0 +1,20 @@
+var num1 = prompt("Enter First Name :");
+var num2 = prompt("Enter Second Name :");
+
+var num1 = parseInt(num1, 10)
+var num2 = parseInt(num2, 10)
+
+var sum = num1 + num2;
+var sub = num1 - num2;
+var multi = num1 * num2;
+var div = num1 / num2;
+var mod = num1 % num2;
+
+
+document.write(num1 + " + " + num2 + " = " + sum + "<br>");
+document.write(num1 + " - " + num2 + " = " + sub + "<br>");
+document.write(num1 + " * " + num2 + " = " + multi + "<br>");
+document.write(num1 + " / " + num2 + " = " + div + "<br>");
+document.write(num1 + " % " + num2 + " = " + mod + "<br>");
+
+
